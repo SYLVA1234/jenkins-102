432c699163ffc1b2c036804402bd9e286a5c4714
SYLVA1234 <142400531+SYLVA1234@users.noreply.github.com>
2024-06-29 18:27:49 +0100

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,2 @@
 
-print("Hello my friends, jenkins is cool but its technical and hard to learn, is not easy to be a Jenkins developer, my name is sylvester")
+print("Hello my friends, jenkins is cool but its technical and hard to learn, is not easy to be a Jenkins developer, my name is sylvester i'm goin to be a jenkins expert")
